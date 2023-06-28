9b625ce7c36a2de298667b819fef5166234737f6
mat-der-D <mat-der-D@users.noreply.github.com>
2023-06-28 23:02:17 +0000

    deploy: 5c80596f67f5fef243a118539a512d0ff54e9737

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"tokyodoves":["",[["prelude",[["board",[],["bitutil.rs","canonicalizer.rs","container.rs","mask.rs","position.rs","route.rs"]]],["board.rs","builder.rs","error.rs","game.rs"]]],["lib.rs","prelude.rs","tools.rs"]]\
+"tokyodoves":["",[["prelude",[["board",[],["bitutil.rs","canonicalizer.rs","container.rs","mask.rs","position.rs","route.rs"]]],["board.rs","builder.rs","error.rs","game.rs","macros.rs"]]],["lib.rs","prelude.rs","tools.rs"]]\
 }');
 createSourceSidebar();
