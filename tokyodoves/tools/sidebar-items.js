f13532a13d5e2e9fbf585d084b545d3db8860e56
mat-der-D <mat-der-D@users.noreply.github.com>
2023-07-06 08:14:23 +0000

    deploy: 7eb7713ea0e0600a675f22efbaab5f1cd6a826c7

--- tokyodoves/tools/sidebar-items.js
+++ tokyodoves/tools/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BoardValue","BoardValueErrorType","CompareBoardValueError"],"fn":["compare_board_value"],"mod":["board_set"],"struct":["LazyBoardLoader","LazyRawBoardLoader"]};
+window.SIDEBAR_ITEMS = {"enum":["BoardValue","BoardValueErrorType","CompareBoardValueError"],"fn":["compare_board_value"],"mod":["board_set"],"struct":["LazyBoardLoader","LazyRawBoardLoader","PositionMapper"]};
