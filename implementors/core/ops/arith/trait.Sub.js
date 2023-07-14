73cbd0ac142fb59468cae82767774a4d23ce4025
mat-der-D <mat-der-D@users.noreply.github.com>
2023-07-14 07:30:59 +0000

    deploy: e1ac5c3290d901348415266264f480903b985d93

--- implementors/core/ops/arith/trait.Sub.js
+++ implementors/core/ops/arith/trait.Sub.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tokyodoves":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"tokyodoves/struct.Shift.html\" title=\"struct tokyodoves::Shift\">Shift</a>&gt; for <a class=\"struct\" href=\"tokyodoves/struct.Shift.html\" title=\"struct tokyodoves::Shift\">Shift</a>"]]
+"tokyodoves":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"tokyodoves/struct.Shift.html\" title=\"struct tokyodoves::Shift\">Shift</a>&gt; for <a class=\"struct\" href=\"tokyodoves/struct.Shift.html\" title=\"struct tokyodoves::Shift\">Shift</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
