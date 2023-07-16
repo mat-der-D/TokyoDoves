705f291488be8e02f5199c684a7681a6730304e9
mat-der-D <mat-der-D@users.noreply.github.com>
2023-07-16 06:50:15 +0000

    deploy: 51bf35897a488e949cba1231b46e15ddff53553b

--- tokyodoves/game/sidebar-items.js
+++ tokyodoves/game/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["GameStatus","Judge"],"struct":["AnalystAgent","Arena","ConsoleAgent","Game","GameRule","RandomAgent"],"trait":["Agent"]};
+window.SIDEBAR_ITEMS = {"enum":["GameDisplayFormat","GameStatus","Judge"],"struct":["AnalystAgent","Arena","ConsoleAgent","Game","GameDisplay","GameRule","RandomAgent"],"trait":["Agent"]};
