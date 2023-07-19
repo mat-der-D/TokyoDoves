d4b51ab25ddcdacc2cd5c5fe6272d0bf52d63d2b
mat-der-D <mat-der-D@users.noreply.github.com>
2023-07-19 02:33:19 +0000

    deploy: 89f62fa2f112af8eb602f1259ff205c61adf9d37

--- implementors/strum/trait.IntoEnumIterator.js
+++ implementors/strum/trait.IntoEnumIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tokyodoves":[["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Color.html\" title=\"enum tokyodoves::Color\">Color</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Dove.html\" title=\"enum tokyodoves::Dove\">Dove</a>"]]
+"tokyodoves":[["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Dove.html\" title=\"enum tokyodoves::Dove\">Dove</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Color.html\" title=\"enum tokyodoves::Color\">Color</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
