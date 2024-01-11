7dc781161d5b299395582593a459f70767dd369c
mat-der-D <mat-der-D@users.noreply.github.com>
2024-01-11 10:30:52 +0000

    deploy: 1f04f9528e7207931383bb4b3bc1d07f02eaae4e

--- implementors/strum/trait.IntoEnumIterator.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"tokyodoves":[["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Dove.html\" title=\"enum tokyodoves::Dove\">Dove</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"tokyodoves/enum.Color.html\" title=\"enum tokyodoves::Color\">Color</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"tokyodoves":["",[["analysis",[],["board_value.rs"]],["collections",[],["board_set.rs","io.rs"]],["prelude",[["board",[],["bitutil.rs","canonicalizer.rs","container.rs","display.rs","main.rs","mask.rs","position.rs","route.rs"]]],["actions.rs","board.rs","builder.rs","macros.rs","pieces.rs","shift.rs"]]],["analysis.rs","collections.rs","error.rs","game.rs","lib.rs","prelude.rs"]]\
+}');
+createSrcSidebar();
